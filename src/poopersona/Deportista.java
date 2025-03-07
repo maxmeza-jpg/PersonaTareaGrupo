fb31c9a89ed26a4c51a0bb20c4d24991879fdfb7
maxmeza-jpg <maxmezahn20@gmail.com>
2025-03-06 21:31:34 -0600

    Update Deportista.java

--- src/poopersona/Deportista.java
+++ src/poopersona/Deportista.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package poopersona;
+package deportista;
 
-/**
- *
- * @author PC
- */
-public class Deportista {
-    
+public class Deportistas extends Persona {
+    private String deporte;
+    private int nivelCompetencia; // 1: Aficionado, 2: Semiprofesional, 3: Profesional
+
+    public Deportistas(String nombre, int edad, String deporte, int nivelCompetencia) {
+        super(nombre = deporte;
+        this.nivelCompetencia = nivelCompetencia;
+    }
+
+    public String getDeporte() {
+        return deporte;
+    }
+
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+
+    public int getNivelCompetencia() {
+        return nivelCompetencia;
+    }
+
+    public void setNivelCompetencia(int nivelCompetencia) {
+        this.nivelCompetencia = nivelCompetencia;
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Edad: " + getEdad());
+        System.out.println("Deporte: " + deporte);
+        System.out.println("Nivel de Competencia: " + nivelCompetencia);
+}
 }
